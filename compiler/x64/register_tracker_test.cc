e2814b5904642209d419b4698705f668c13cc31f
Tyler Rockwood <rockwotj@users.noreply.github.com>
2023-07-02 22:12:38 -0500

    Add utilities for x64 compiler (#4)
    When compiling code we'll need to keep track of the current spot in stack memory as well as where registers are allocated.

---
+++ compiler/x64/register_tracker_test.cc
@@ -0,0 +1,49 @@
+#include "compiler/x64/register_tracker.h"
+
+#include <gtest/gtest-matchers.h>
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <optional>
+#include <vector>
+
+#include "absl/container/flat_hash_set.h"
+#include "gmock/gmock.h"
+
+template <>
+struct std::hash<asmjit::x86::Gp> {
+  std::size_t operator()(asmjit::x86::Gp const& reg) const noexcept {
+    std::size_t h1 = std::hash<uint32_t>{}(reg.id());
+    std::size_t h2 = std::hash<uint32_t>{}(reg.signature().bits());
+    return h1 ^ (h2 << 1U);
+  }
+};
+
+namespace wasmcc::x64 {
+
+TEST(RegisterTracker, AllocatesRegisters) {
+  RegisterTracker rt;
+  auto r1 = rt.TakeUnusedRegister();
+  EXPECT_NE(r1, std::nullopt);
+  auto r2 = rt.TakeUnusedRegister();
+  EXPECT_NE(r2, std::nullopt);
+  EXPECT_NE(r1, r2);
+}
+
+TEST(RegisterTracker, CanAllocateAllRegisters) {
+  RegisterTracker rt;
+  std::vector<asmjit::x86::Gp> registers;
+  while (true) {
+    auto reg = rt.TakeUnusedRegister();
+    if (!reg.has_value()) {
+      break;
+    }
+    registers.push_back(*reg);
+  }
+  absl::flat_hash_set<asmjit::x86::Gp> unique(registers.begin(),
+                                              registers.end());
+  EXPECT_THAT(registers, testing::UnorderedElementsAreArray(unique));
+}
+}  // namespace wasmcc::x64
---
+++ compiler/x64/runtime_stack.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <span>
+
+#include "absl/container/fixed_array.h"
+#include "compiler/x64/register_tracker.h"
+#include "core/value.h"
+
+namespace wasmcc::x64 {
+
+struct RuntimeValue {
+  // The offset relative to the base of the stack in memory where this stack
+  // value can spill to if not in a register.
+  int64_t stack_pointer = 0;
+  // The register that this value is located in.
+  std::optional<asmjit::x86::Gp> reg;
+  // The type of this register
+  ValType type = ValType::kI32;
+
+  bool operator==(const RuntimeValue&) const = default;
+
+  // The size of this value on the stack in bytes.
+  size_t size_bytes() const;
+};
+
+/**
+ * This class tracks what is on the stack during compilation.
+ *
+ * It also is responsible for tracking stack value's location in stack memory.
+ */
+class RuntimeStack {
+  using UnderlyingType = absl::FixedArray<RuntimeValue>;
+
+ public:
+  RuntimeStack(size_t max_stack_elements, RegisterTracker*);
+  RuntimeStack(const RuntimeStack&) = delete;
+  RuntimeStack& operator=(const RuntimeStack&) = delete;
+  RuntimeStack(RuntimeStack&&) = delete;
+  RuntimeStack& operator=(RuntimeStack&&) = delete;
+  ~RuntimeStack() = default;
+
+  RuntimeValue* Push(RuntimeValue);
+  [[nodiscard]] RuntimeValue Pop();
+  RuntimeValue* Peek();
+  const RuntimeValue* Peek() const;
+
+  std::span<const RuntimeValue> ReverseIterator() const;
+
+  // The current offset in bytes from the bottom of current function's stack.
+  size_t pointer() const noexcept { return _stack_memory_offset; }
+
+ private:
+  RegisterTracker* _reg_tracker;
+  // The current pointer to the stack in memory, relative to the top of the
+  // stack for this calling frame.
+  int64_t _stack_memory_offset{0};
+  // The current stack size
+  size_t _stack_size{0};
+  UnderlyingType _stack;
+};
+
+}  // namespace wasmcc::x64
