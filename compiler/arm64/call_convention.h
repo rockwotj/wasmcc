c0dee6e0452b87f867332edb7d20a6757b45fbc7
Tyler Rockwood <rockwotj@users.noreply.github.com>
2023-07-07 22:03:40 -0500

    Introduce calling convention (#10)
    This struct can be used to specify the calling convention, registers used
    and other cross architecture concerns.
    
    Next up we should pull out a function frame helper cross platform, as we're doing the same thing,
    but using different registers etc.

---
+++ compiler/arm64/call_convention.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <asmjit/a64.h>
+
+#include <array>
+#include <initializer_list>
+
+#include "compiler/common/call_convention.h"
+
+namespace wasmcc::arm64 {
+using GpReg = asmjit::a64::Gp;
+using VecReg = asmjit::a64::Vec;
+constexpr GpReg kSpReg = asmjit::a64::sp;
+
+// The AArch64 calling convention, it's the same on all platforms 🙌
+struct CallingConvention {
+  using GpReg = GpReg;
+  using VecReg = VecReg;
+  static constexpr GpReg kSp = kSpReg;
+  static constexpr std::array kGpArgs = {
+      asmjit::a64::x0, asmjit::a64::x1, asmjit::a64::x2, asmjit::a64::x3,
+      asmjit::a64::x4, asmjit::a64::x5, asmjit::a64::x6, asmjit::a64::x7,
+  };
+  static constexpr std::array kVecArgs = {
+      asmjit::a64::v0, asmjit::a64::v1, asmjit::a64::v2, asmjit::a64::v3,
+      asmjit::a64::v4, asmjit::a64::v5, asmjit::a64::v6, asmjit::a64::v7,
+  };
+  static constexpr std::array kGpRets = kGpArgs;
+  static constexpr std::array kVecRets = kVecArgs;
+  const static RegisterMask<GpReg> kGpCalleeSavedRegisters;
+  // NOTE: only the bottom 64 bits are preserved.
+  const static RegisterMask<VecReg> kVecCalleeSavedRegisters;
+  const static RegisterMask<GpReg> kGpCallerSavedRegisters;
+  const static RegisterMask<VecReg> kVecCallerSavedRegisters;
+
+  static constexpr size_t kStackAlignment = 16;
+};
+
+}  // namespace wasmcc::arm64
--- compiler/arm64/register_tracker.h
+++ compiler/arm64/register_tracker.h
@@ -6,55 +6,11 @@
 #include <bitset>
 #include <optional>
 
+#include "compiler/arm64/call_convention.h"
+#include "compiler/common/register_tracker.h"
+
 namespace wasmcc::arm64 {
 
-/**
- * This class tracks if a given register is in use.
- *
- * In the future this class will also likely need to track **where** a register
- * is in use. So we don't have to do things like always save arguments to the
- * stack.
- */
-class RegisterTracker {
- public:
-  RegisterTracker() = default;
-  RegisterTracker(const RegisterTracker&) = delete;
-  RegisterTracker& operator=(const RegisterTracker&) = delete;
-  RegisterTracker(RegisterTracker&&) = delete;
-  RegisterTracker& operator=(RegisterTracker&&) = delete;
-  ~RegisterTracker() = default;
+using RegisterTracker = wasmcc::RegisterTracker<CallingConvention>;
 
-  /**
-   * Currently, we only use caller saved registers, but we should certainly also
-   * be able to use save callee registers too to resolve some register pressure.
-   */
-  static constexpr std::array kCallerSavedRegisters = {
-      // function args and results
-      asmjit::a64::regs::x0,
-      asmjit::a64::regs::x1,
-      asmjit::a64::regs::x2,
-      asmjit::a64::regs::x3,
-      asmjit::a64::regs::x4,
-      asmjit::a64::regs::x5,
-      asmjit::a64::regs::x6,
-      asmjit::a64::regs::x7,
-      // caller saved
-      asmjit::a64::regs::x9,
-      asmjit::a64::regs::x10,
-      asmjit::a64::regs::x11,
-      asmjit::a64::regs::x12,
-      asmjit::a64::regs::x13,
-      asmjit::a64::regs::x14,
-      asmjit::a64::regs::x15,
-  };
-  static constexpr size_t kNumGeneralPurposeRegisters = 32;
-  using RegisterUsedMask = std::bitset<kNumGeneralPurposeRegisters>;
-
-  std::optional<asmjit::a64::Gp> TakeUnusedRegister();
-
-  void MarkRegisterUnused(const asmjit::a64::Gp&);
-
- private:
-  RegisterUsedMask _registers_used_mask;
-};
 }  // namespace wasmcc::arm64
