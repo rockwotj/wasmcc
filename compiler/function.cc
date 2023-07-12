f87dc6b1737d6aaf2e8265292bc7f711c2a98ec3
Tyler Rockwood <rockwood@redpanda.com>
2023-07-12 06:01:37 -0500

    Add basic WASM VM interface

--- compiler/function.cc
+++
@@ -1,7 +0,0 @@
-#include "compiler/function.h"
-
-namespace wasmcc {
-CompiledFunction::CompiledFunction(void* p) : _ptr(p) {}
-
-void* CompiledFunction::get() const { return _ptr; }
-}  // namespace wasmcc
