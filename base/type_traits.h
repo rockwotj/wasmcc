e3ac625b1c034c1f9c104b97cb7039533654ef38
Tyler Rockwood <rockwotj@users.noreply.github.com>
2023-07-14 10:05:38 -0500

    Add basic WASM VM interface (#14)

--- base/type_traits.h
+++ base/type_traits.h
@@ -1,4 +1,7 @@
+#pragma once
+#include <tuple>
 #include <type_traits>
+#include <utility>
 
 namespace wasmcc {
 template <typename>
@@ -6,5 +9,19 @@ struct DependantFalse : std::false_type {};
 
 template <typename... Args>
 concept EmptyPack = sizeof...(Args) == 0;
+
+template <typename T>
+struct FunctionTraits : public FunctionTraits<decltype(&T::operator())> {};
+
+template <typename ReturnType, typename... Args>
+struct FunctionTraits<ReturnType(Args...)> {
+  using result_type = ReturnType;
+  using arg_types = std::tuple<Args...>;
+};
+template <typename ReturnType, typename... Args>
+struct FunctionTraits<ReturnType (*)(Args...)> {
+  using result_type = ReturnType;
+  using arg_types = std::tuple<Args...>;
+};
 
 }  // namespace wasmcc
